deb012924cd35f9bb488d2ef4eb765045a4c29d6
1B21NS <114583423+Nataliaass@users.noreply.github.com>
2022-10-05 12:12:16 +0000

    zrobione

--- kartapracy1.py
+++ kartapracy1.py
@@ -2,8 +2,30 @@
 a= int(input())
 b= int(input())
 print(a**2 + b**2)
- #zad2
 
+ #zad 2
 a= int(input())
 b= int(input())
 print((a+b)**2)
+
+# zad 3
+a= int(input())
+b=int(input())
+print(a-b(**3))
+
+#zad 4
+a,b,c=map(int, input().split())
+print(a*b*c)
+
+#zad 5
+a, b = int(input()), int(input())
+a=1,2345
+print(2*(a+b))
+
+# zad 6
+brutto= float(input())
+print(brutto / 1.23)
+print(brutto2/ 1,23)
+# zad 7
+a,b = int(input())
+print(a%b)
