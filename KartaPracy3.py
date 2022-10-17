825a2436505ce56a0e45fa705dbf30120627b76f
1B21NS <114583423+Nataliaass@users.noreply.github.com>
2022-10-17 10:20:02 +0000

    zaczety

---
+++ KartaPracy3.py
@@ -0,0 +1,10 @@
+
+for i in range(3, 10):
+    print(i)
+
+for i in range(10, 22):
+    print(i)
+  
+for i in range(15, 32):
+     print(i)
+  
---
+++ temp.py
@@ -0,0 +1,9 @@
+#zad 3
+k=int(input())
+l=int(input())
+m=int(input())
+if k=l
+print("TAK")
+else:
+print("NIE")
+      
