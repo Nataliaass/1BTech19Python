56536c88c80a7a9ebaadb73de3a3e020be6ab443
1B21NS <114583423+Nataliaass@users.noreply.github.com>
2022-10-19 12:14:59 +0000

    dokonczyc

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -3,8 +3,33 @@ for i in range(3, 10):
     print(i)
 
 for i in range(10, 22):
-    print(i)
+    print(i, end=" ")
   
-for i in range(15, 32):
+for i in range(15,33,2):
      print(i)
+
+for i in range(99,9,-1):
+  print(i, end= " ")
+
+for i in range(60,10):
+  print(i)
+
+for i in range(100,1000,20):
+  print(i, end=" ")
+
+print()
+print()
+for i in range(5,50):
+  print(i*20, end=" ")
   
+#Zad 1
+  n= int(input())
+  
+for i in range(n):
+  print(i**3 + 3, end=" ")
+#zad2
+for i in range(105,1000,15):
+  if i % 15 == 0 :
+  print(i, end=" ")
+
+#zad3
