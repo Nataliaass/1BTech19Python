3a0c6d6156758ced2d205f2795050457251cfbb2
1B21NS <114583423+Nataliaass@users.noreply.github.com>
2022-12-14 13:14:37 +0000

    karta pracy

---
+++ Karty Pracy/KartaPracy6.py
@@ -0,0 +1,53 @@
+# #zad 1
+# a= int(input())
+# b=int(input())
+# c= int(input())
+# #arytm
+# if b - a == c - b: print("C jest arytmetyczny")
+
+# #geom
+# if b / a == c/ b: print("Ciąg jest geometryczny")
+
+# #rosnący
+#   if a< b< c: print("ciąg jest rosnący ")
+
+  
+#zad 2
+
+# suma=0
+# for i in range(100,1000):
+#  if i % 8== 0 and i % 16 != 0:
+#    suma= suma
+# print(suma)
+
+# #zad3
+# for i in range(99,9,-1):
+#   if i % 7==0:
+#     wielok = i
+#     break
+# ilość= 0 
+# for i in range(1000,1000):
+#   if i % wielok==0:
+#     ilość= ilość +1
+# print(ilość)
+
+#zad 4
+
+# for i in range(10,100):
+    
+#   cd = i // 10
+#   cj = i % 10
+#   if cd >= 2 * cj:
+#     ilość +=1
+# print(ilość)
+
+#zad 5
+# ilość= 0
+#  suma=0
+#  for i in range(100,1000):
+#    cs = i// 100 
+#   cd = (i // 100)//10
+# cd= (i//10) % 10
+#   cj = i % 10
+#   
+#  print(ilość)
